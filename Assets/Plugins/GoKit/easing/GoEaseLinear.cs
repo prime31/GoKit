dabf2bb78ae171a5a88dfd353da51898b60d17af
Mike <mike@prime31.com>
2013-01-10 17:49:54 -0800

    added Punch

--- Assets/Plugins/GoKit/easing/GoEaseLinear.cs
+++ Assets/Plugins/GoKit/easing/GoEaseLinear.cs
@@ -4,4 +4,17 @@
     {
         return c * t / d + b;
     }
+	
+	
+    public static float Punch( float t, float b, float c, float d )
+    {
+        if( t == 0 )
+            return 0;
+
+        if( ( t /= d ) == 1 )
+            return 0;
+ 
+        const float p = 0.3f;
+        return ( c * Mathf.Pow( 2, -10 * t ) * Mathf.Sin( t * ( 2 * Mathf.PI ) / p ) );
+    }
 }
