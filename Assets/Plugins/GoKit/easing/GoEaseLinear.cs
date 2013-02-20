3eca99e888856a5fe81a34a718fe39f2d766df66
Mike <mike@prime31.com>
2013-02-19 22:23:18 -0800

    just ran through the formatter

--- Assets/Plugins/GoKit/easing/GoEaseLinear.cs
+++ Assets/Plugins/GoKit/easing/GoEaseLinear.cs
@@ -3,8 +3,8 @@
 
 public static class GoEaseLinear
 {
-    public static float EaseNone( float t, float b, float c, float d )
-    {
-        return c * t / d + b;
-    }
+	public static float EaseNone( float t, float b, float c, float d )
+	{
+		return c * t / d + b;
+	}
 }
