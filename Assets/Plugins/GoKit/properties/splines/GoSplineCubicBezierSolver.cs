ecfff526b5f683c9608a63d3a2e138427e38b74a
Tomasz Tunik <tomasztunik@gmail.com>
2014-03-27 02:59:56 +0100

    Fixed bug in GoSplineCubicBezierSolver where reversing a path with 4 nodes would give unexpected results because of the nodes order being mixed up - automatically fixes it for the GoDummyPathEditor which uses the solver for drawing

--- Assets/Plugins/GoKit/properties/splines/GoSplineCubicBezierSolver.cs
+++ Assets/Plugins/GoKit/properties/splines/GoSplineCubicBezierSolver.cs
@@ -25,7 +25,7 @@ public class GoSplineCubicBezierSolver : AbstractGoSplineSolver
 	public override Vector3 getPoint( float t )
 	{
 		float d = 1f - t;
-		return d * d * d * _nodes[0] + 3f * d * d * t * _nodes[2] + 3f * d * t * t * _nodes[3] + t * t * t * _nodes[1];
+		return d * d * d * _nodes[0] + 3f * d * d * t * _nodes[1] + 3f * d * t * t * _nodes[2] + t * t * t * _nodes[3];
 	}
 
 	
@@ -35,8 +35,8 @@ public class GoSplineCubicBezierSolver : AbstractGoSplineSolver
 		var originalColor = Gizmos.color;
 		Gizmos.color = Color.red;
 		
-		Gizmos.DrawLine( _nodes[0], _nodes[2] );
-		Gizmos.DrawLine( _nodes[3], _nodes[1] );
+		Gizmos.DrawLine( _nodes[0], _nodes[1] );
+		Gizmos.DrawLine( _nodes[2], _nodes[3] );
 		
 		Gizmos.color = originalColor;
 	}
