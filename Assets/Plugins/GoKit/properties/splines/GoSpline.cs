2268b2e2f23da5d701fc489e7fa50f4459cab271
Mike <mike@prime31.com>
2014-05-13 17:07:21 -0700

    fixed an oopsie on Android when loading GoSplines from disk.

--- Assets/Plugins/GoKit/properties/splines/GoSpline.cs
+++ Assets/Plugins/GoKit/properties/splines/GoSpline.cs
@@ -76,7 +76,7 @@ public class GoSpline
 			path = Path.Combine( "jar:file://" + Application.dataPath + "!/assets/", pathAssetName );
 		
 	        WWW loadAsset = new WWW( path );
-	        while( loadAsset.isDone ) { } // maybe make a safety check here
+	        while( !loadAsset.isDone ) { } // maybe make a safety check here
 			
 			return bytesToVector3List( loadAsset.bytes );
 		}
