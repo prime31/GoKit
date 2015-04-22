d7e89d47feabd8019972c99dfd97268ab8415f38
Ryan Goodrich <ryan@Ryans-MacBook-Pro.local>
2015-04-21 17:14:42 -0700

    changed GoDataType into GoProxyProp

---
+++ Assets/Plugins/GoKit/GoProxyProp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoProxyProp<T>  
+{
+
+	public T value { get; set; }
+	
+	public GoProxyProp( T startValue )
+	{
+		value = startValue;
+	}                                                   
+    
+}
