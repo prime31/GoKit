3e2c923cfc92b1c2de7a8a9dde22e55d332f7c61
Greg Harding <greg@flightless.co.nz>
2015-08-05 15:34:46 +1200

    Increased speed of generating and querying splines (excluding straight-line solver).

--- Assets/Plugins/GoKit/properties/splines/AbstractGoSplineSolver.cs
+++ Assets/Plugins/GoKit/properties/splines/AbstractGoSplineSolver.cs
@@ -20,26 +20,46 @@ public abstract class AbstractGoSplineSolver
 	// how many subdivisions should we divide each segment into? higher values take longer to build and lookup but
 	// result in closer to actual constant velocity
 	protected int totalSubdivisionsPerNodeForLookupTable = 5;
-	protected Dictionary<float, float> _segmentTimeForDistance; // holds data in the form [time:distance] as a lookup table
-	
-	
-	
+
+	// time:distance lookup table
+	protected struct Segment
+	{
+		public float time;
+		public float distance;
+
+		public Segment ( float time, float distance )
+		{
+			this.time = time;
+			this.distance = distance;
+		}
+	}
+
+	protected List<Segment> segments;
+
+
 	// the default implementation breaks the spline down into segments and approximates distance by adding up
 	// the length of each segment
 	public virtual void buildPath()
 	{
-		var totalSudivisions = _nodes.Count * totalSubdivisionsPerNodeForLookupTable;
+		// build or clear segments cache
+		var totalSubdivisions = _nodes.Count * totalSubdivisionsPerNodeForLookupTable;
+		if( segments == null )
+		{
+			segments = new List<Segment>(totalSubdivisions);
+		}
+		else
+		{
+			segments.Clear();
+			segments.Capacity = totalSubdivisions;
+		}
+
 		_pathLength = 0;
-		float timePerSlice = 1f / totalSudivisions;
-		
-		// we dont care about the first node for distances because they are always t:0 and len:0
-		_segmentTimeForDistance = new Dictionary<float, float>( totalSudivisions );
-		
-		
+		float timePerSlice = 1f / totalSubdivisions;
 		var lastPoint = getPoint( 0 );
 		
 		// skip the first node and wrap 1 extra node
-        for( var i = 1; i < totalSudivisions + 1; i++ )
+		// we dont care about the first node for distances because they are always t:0 and len:0
+        for( var i = 1; i < totalSubdivisions + 1; i++ )
         {
 			// what is the current time along the path?
             float currentTime = timePerSlice * i;
@@ -48,7 +68,8 @@ public abstract class AbstractGoSplineSolver
             _pathLength += Vector3.Distance( currentPoint, lastPoint );
             lastPoint = currentPoint;
 
-            _segmentTimeForDistance.Add( currentTime, _pathLength );
+			// cache segment
+			segments.Add(new Segment(currentTime, _pathLength));
         }
 	}
 	
@@ -65,44 +86,34 @@ public abstract class AbstractGoSplineSolver
 	public virtual Vector3 getPointOnPath( float t )
 	{
 		// we know exactly how far along the path we want to be from the passed in t
-		var targetDistance = _pathLength * t;
-		
-		// store the previous and next nodes in our lookup table
-		var previousNodeTime = 0f;
-		var previousNodeLength = 0f;
-		var nextNodeTime = 0f;
-		var nextNodeLength = 0f;
-
-		float[] keysSegmentTimeForDistance = new float[_segmentTimeForDistance.Keys.Count];
-		_segmentTimeForDistance.Keys.CopyTo ( keysSegmentTimeForDistance, 0 );
+		float targetDistance = _pathLength * t;
 
 		// loop through all the values in our lookup table and find the two nodes our targetDistance falls between
-		for( int k = 0; k < keysSegmentTimeForDistance.Length; ++k )
-		{
-			float key = keysSegmentTimeForDistance[k];
-			float value = _segmentTimeForDistance[key];
-
-			// have we passed our targetDistance yet?
-		    if( value >= targetDistance )
-		    {
-		        nextNodeTime = key;
-		        nextNodeLength = value;
-				
-		        if( previousNodeTime > 0 )
-		            previousNodeLength = _segmentTimeForDistance[previousNodeTime];
-
-		        break;
-		    }
-		    previousNodeTime = key;
-		}
-		
 		// translate the values from the lookup table estimating the arc length between our known nodes from the lookup table
-		var segmentTime = nextNodeTime - previousNodeTime;
-		var segmentLength = nextNodeLength - previousNodeLength;
-		var distanceIntoSegment = targetDistance - previousNodeLength;
-		
-		t = previousNodeTime + ( distanceIntoSegment / segmentLength ) * segmentTime;
-		
+		int nextSegmentIndex;
+		for( nextSegmentIndex = 0; nextSegmentIndex < segments.Count; nextSegmentIndex++ )
+		{
+			if( segments[nextSegmentIndex].distance >= targetDistance )
+				break;
+		}
+
+		Segment nextSegment = segments[nextSegmentIndex];
+
+		if( nextSegmentIndex == 0 ) {
+			// t within first segment
+			t = ( targetDistance / nextSegment.distance ) * nextSegment.time;
+		}
+		else
+		{
+			// t within prev..next segment
+			Segment previousSegment = segments[nextSegmentIndex-1];
+
+			float segmentTime = nextSegment.time - previousSegment.time;
+			float segmentLength = nextSegment.distance - previousSegment.distance;
+
+			t = previousSegment.time + ( ( targetDistance - previousSegment.distance ) / segmentLength ) * segmentTime;
+		}
+
 		return getPoint( t );
 	}
 	
