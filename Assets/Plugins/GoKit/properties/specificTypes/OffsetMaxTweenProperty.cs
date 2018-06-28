83170542661d92d5056d162291d6937c023efed8
Dmitry Petukhov <virtualthief@mail.ru>
2018-06-28 22:07:22 +0300

    Fixed simultaneous tween of RectTransform offsetMin and offsetMax

---
+++ Assets/Plugins/GoKit/properties/specificTypes/OffsetMaxTweenProperty.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public sealed class OffsetMaxTweenProperty : AbstractOffsetTweenProperty
+{
+    public OffsetMaxTweenProperty( Vector2 endValue, bool isRelative = false ) : base( endValue, isRelative, useMax: true )
+    {
+    }
+}
