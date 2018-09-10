a5fc8e0c0b547bf7916e1d47599cea9b57b98057
Mike <mike@prime31.com>
2018-09-10 12:58:52 -0700

    very basic ActionTweenProperty added

---
+++ Assets/Plugins/GoKit/properties/genericTypes/ActionTweenProperty.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// simple class that takes in an Action and calls it each tick with the eased value from startValue to endValue
+/// </summary>
+public class ActionTweenProperty : AbstractTweenProperty
+{
+	System.Action<float> _action;
+	
+	float _startValue;
+	float _endValue;
+
+
+	public ActionTweenProperty( System.Action<float> action, float startValue = 0, float endValue = 1 )
+	{
+		_action = action;
+		_startValue = startValue;
+		_endValue = endValue;
+	}
+
+
+	public override void prepareForUse()
+	{}
+
+
+	public override void tick( float totalElapsedTime )
+	{
+		var easedValue = _easeFunction( totalElapsedTime, _startValue, _endValue, _ownerTween.duration );
+		_action( easedValue );
+	}
+}
